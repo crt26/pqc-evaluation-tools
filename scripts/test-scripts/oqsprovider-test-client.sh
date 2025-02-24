9819279a951cf729fefa211812d3d8586fdec0a8
crt26 <callumturino@hotmail.co.uk>
2025-02-24 18:06:43 +0000

    Remove debug statements

--- scripts/test-scripts/oqsprovider-test-client.sh
+++ scripts/test-scripts/oqsprovider-test-client.sh
@@ -165,16 +165,13 @@ function check_control_port() {
     # Helper function for checking if the control port is open and listening on the other testing machine. It will continuously check
     # until the port is open and listening before returning exiting the function, allowing the control_signal function to send the signal.
 
-    # echo "Checking if target control port is open"
-
     # # Wait until the server is listening on the control port before sending signal
     # until nc -z "$SERVER_IP" 12345 > /dev/null 2>&1; do
     #     :
     # done
 
+    # Small delay before sending signal to allow target device to open port and listen
     sleep 0.3
-
-    # echo "Target control port is open"
 
 }
 
@@ -194,13 +191,8 @@ function control_signal() {
 
         "control_send")
 
-            # echo "****************************************"
-            # echo "[DEBUG] - Initiating control send signal to server"
-
             # Check if the control port is open on the server before sending signal
             check_control_port
-
-            # echo "Sending control signal to the server with the message - $message"
 
             # Send control signal to the server until successful
             until echo "$message" | nc -n -w 1 "$SERVER_IP" 12345 > /dev/null 2>&1; do
@@ -211,14 +203,9 @@ function control_signal() {
                     break
                 fi
             done
-
-            # echo -e "Control signal sent to server\n"
             ;;
 
         "control_wait")
-
-            # echo "****************************************"
-            # echo "[DEBUG] - Initiating control wait signal from server"
 
             # Wait until the control signal has been received and return the message
             while true; do
@@ -232,19 +219,13 @@ function control_signal() {
                 fi
 
             done
-            # echo -e "Control signal received from server with the message - $signal_message\n"
             ;;
 
         "iteration_handshake")
 
-            # echo -e "\n****************************************"
-            # echo "[DEBUG] - Initiating iteration handshake signal"
-
-            # echo "Preparing to send handshake_ready signal to server"
             # Check if the control port is open on the server before sending signal
             check_control_port
 
-            # echo "Sending handshake_ready signal to server"
             # Send control signal to the server until successful
             until echo "handshake_ready" | nc -n -w 1 "$SERVER_IP" 12345 > /dev/null 2>&1; do
                 exit_status=$?
@@ -254,9 +235,7 @@ function control_signal() {
                     break
                 fi
             done
-            # echo "handshake_ready signal sent to server successfully"
 
-            # echo "Waiting for server to send handshake_ready signal"
             # Wait for the server to send ready signal
             while true; do
                 signal_message=$(nc -l -p 12346)
@@ -264,12 +243,10 @@ function control_signal() {
                     break
                 fi
             done
-
-            # echo -e "Received handshake_ready signal from server, handshake complete\n"
             ;;
 
         *)
-            echo "Unknown type: $type"
+            echo "[ERROR] - Unknown control signal type: $type"
             exit 1
             ;;
 
@@ -334,7 +311,6 @@ function pqc_tests() {
                         # Debug line for checking server connection with current sig/kem combination. To use uncomment line and then comment out the s_time line
                         #"$openssl_path/bin/openssl" s_client -connect $SERVER_IP:4433 -CAfile $cert_file -provider default -provider oqsprovider -provider-path $provider_path -groups "$kem"
 
-                        # echo "[DEBUG] - Running client test process"
                         # Running OpenSSL s_time process with current test parameters
                         "$openssl_path/bin/openssl" s_time -connect $SERVER_IP:4433 -CAfile $cert_file -time $TIME_NUM  -verify 1 \
                             -provider default -provider oqsprovider -provider-path $provider_path > $handshake_dir/$output_name
@@ -423,7 +399,6 @@ function classic_tests() {
                 # Performing testing until successful or fail counter reaches limit
                 while true; do
 
-                    # echo "[DEBUG] - Running client test process"
                     # Running OpenSSL s_time process with current test parameters
                     "$openssl_path/bin/openssl" s_time -connect $SERVER_IP:4433 -CAfile $classic_cert_file -time $TIME_NUM > "$CLASSIC_HANDSHAKE/$output_name"
                     exit_code=$?
